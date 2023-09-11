aba7ab6c2ef28f53a0274dbc912fe15e0af2bc5a
gabigutu <gabi.gutu@gmail.com>
2023-09-11 15:53:25 +0300

    Practice JS

--- practice_01/index.js
+++ practice_01/index.js
@@ -1,62 +1,99 @@
- const moviestable = document.getElementById('movies');
+const moviestable = document.getElementById('movies');
 
-    const movies = [
-        {title: 'Fromula1', description: 'best sport', rating: 5,actors: ['hamilton', 'verstapen', 'vettel']},
-        {title: 'Fotbal', description: 'Just Money', rating: 4.7,actors: ['messi', 'ronaldo', 'neymar']},
-        {title: 'Tennis', description: 'Nice Sport', rating: 3,actors: ['djokovic', 'federer', 'nadal']},
-    
-    ];
+const movies = [
+    { title: 'Fromula1', description: 'best sport', rating: 5, actors: ['hamilton', 'verstapen', 'vettel'] },
+    { title: 'Fotbal', description: 'Just Money', rating: 4.7, actors: ['messi', 'ronaldo', 'neymar'] },
+    { title: 'Tennis', description: 'Nice Sport', rating: 3, actors: ['djokovic', 'federer', 'nadal'] },
 
-    function adauga (){
+];
 
-        for (aux of movies){
-            const tr = document.createElement('tr');
-            const tdTitle = document.createElement('td');
-            const tdDescription = document.createElement('td');
-            const tdRating = document.createElement('td');
-            const tdActors = document.createElement('td');
+function adauga(movies) {
+    console.log(movies);
 
-            const tdDelete = document.createElement('td');
-            const deleteButton = document.createElement('button');
-            deleteButton.innerText = 'Delete';
-            
+    for (aux of movies) {
+        const tr = document.createElement('tr');
+        const tdTitle = document.createElement('td');
+        const tdDescription = document.createElement('td');
+        const tdRating = document.createElement('td');
+        const tdActors = document.createElement('td');
 
-
-            deleteButton.addEventListener('click', function(){
-                tr.remove();
-            });
-            tdDelete.appendChild(deleteButton);
-            tr.appendChild(tdDelete);
+        const tdDelete = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
 
 
 
-            tdTitle.innerText = aux.title;
-            tdDescription.innerText = aux.description;
-            tdRating.innerText = aux.rating;
-            for (actor of aux.actors){
-                tdActors.innerText += actor + ' , ';
+        deleteButton.addEventListener('click', function () {
+            tr.remove();
+        });
+        tdDelete.appendChild(deleteButton);
+
+
+
+        tdTitle.innerText = aux.title;
+        tdDescription.innerText = aux.description;
+        tdRating.innerText = aux.rating;
+        for (actor of aux.actors) {
+            tdActors.innerText += actor + ',';
+        }
+
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdDescription);
+        tr.appendChild(tdRating);
+        tr.appendChild(tdActors);
+        tr.appendChild(tdDelete);
+
+        moviestable.appendChild(tr);
+    }
+}
+
+const adaugaFilmeButton = document.getElementById('adaugaFilme');
+adaugaFilmeButton.addEventListener('click', function () {
+    adauga(movies);
+});
+
+function sortare() {
+    const selVal = document.getElementById('select').value;
+    switch (selVal) {
+        case 'title': movies.sort(function (a, b) { return a.title.localeCompare(b.title); }); break;
+        case 'rating': sortareRating(); break;
+    }
+}
+
+function sortareRating() {
+    // movies.sort(function (a, b) { return a.rating - b.rating; });
+    const movieRows = moviestable.getElementsByTagName('tr');
+    const currentMovies = [];
+    for (movie of movieRows) {
+        if (movie.classList.contains('header-row')) continue;
+        console.log(movie, typeof movie);
+        currentMovies.push({
+            title: movie.getElementsByTagName('td')[0].innerText,
+            description: movie.getElementsByTagName('td')[1].innerText,
+            actors: movie.getElementsByTagName('td')[3].innerText.split(','),
+            rating: movie.getElementsByTagName('td')[2].innerText
+        });
+    }
+    // currentMovies.splice(0, 1);
+    // console.log(currentMovies);
+
+    let auFostSchimbari;
+    do {
+        auFostSchimbari = false;
+        for (let i = 0; i < currentMovies.length - 1; i++) {
+            if (currentMovies[i].rating > currentMovies[i + 1].rating) {
+                auFostSchimbari = true;
+                [currentMovies[i], currentMovies[i + 1]] = [currentMovies[i + 1], currentMovies[i]];
             }
-
-            tr.appendChild(tdTitle);
-            tr.appendChild(tdDescription);
-            tr.appendChild(tdRating);
-            tr.appendChild(tdActors);
-
-            moviestable.appendChild(tr);
-
         }
-             
-
-
-    }
-
-    function sortare(){
-
-        
-
-        const selVal = document.getElementById('select').value;
-        switch(selVal){
-            case 'title': movies.sort(function(a,b){ return a.title.localeCompare(b.title);}); break;
-            case 'rating': movies.sort(function(a,b){ return a.rating - b.rating;}); break;
-        }
-    }
+    } while (auFostSchimbari);
+    console.log(currentMovies);
+    moviestable.innerHTML = '<tr class="header-row"> ' +
+        '<td>Title</td>' +
+        '<td>Description</td> ' +
+        '<td>Acthors</td>' +
+        '<td>Rating</td>' +
+        '<td>Actions</td>' +
+        '</tr>';
+    adauga(currentMovies);
+}
